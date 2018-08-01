b9be5fc8bdae737bb46501b090d11dcc2f423563
Unknown <mythologyali@gmail.com>
2018-08-01 14:42:15 -0700

    better logging for downloader

--- server/youtube/download.py
+++ server/youtube/download.py
@@ -4,9 +4,9 @@ from utils.logging import logger
 
 
 async def fetch_info(client: youtube_dl.YoutubeDL, song: str, ip: str = 'unknown user') -> dict:
-    logger.info(f'Got an extract request for {song} from {ip}')
+    logger.info(f'Got an extract request for "{song}" from [{ip}]')
     start = datetime.now()
-    something = client.extract_info(f'ytsearch:{song}', download=False)
+    info = client.extract_info(f'ytsearch:{song}', download=False)
     delta = datetime.now() - start
-    logger.info(f'Extracted information for {song} in {delta}ms')
-    return something
+    logger.info(f'Extracted information for "{song}" in {delta.seconds} seconds')
+    return info
