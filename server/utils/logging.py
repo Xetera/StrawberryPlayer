80a6876445b6a91f1b361c26765d8db47e217a36
Unknown <mythologyali@gmail.com>
2018-08-01 14:36:36 -0700

    changed: better logging + file handler

--- server/utils/logging.py
+++ server/utils/logging.py
@@ -1,9 +1,30 @@
 import logging
 import os
 
-ip = os.environ.get('IP_ADDRESS', '127.0.0.1')
-FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
-logging.basicConfig(format=FORMAT)
-d = {'clientip': ip, 'user': 'Strawberry-Server'}
 
-logger = logging.getLogger('tcpserver')
+def create_logger():
+    format = '%(asctime)-15s [%(levelname)s]: %(message)s'
+
+    fh = logging.FileHandler('server.log')
+    fh.setLevel('DEBUG')
+
+    ch = logging.StreamHandler()
+    ch.setLevel('ERROR')
+    formatter = logging.Formatter(format)
+    logging.basicConfig(format=format)
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+
+    out = logging.getLogger('StrawberryServer')
+
+    log_level = os.environ.get('STRAWBERRY_LOG_LEVEL', 'INFO')
+    if log_level not in ['DEBUG', 'INFO', 'WARNING', ]:
+        raise EnvironmentError("STRAWBERRY_LOG_LEVEL environment set but is not valid")
+
+    out.setLevel(log_level)
+    out.addHandler(fh)
+    out.addHandler(ch)
+    return out
+
+
+logger = create_logger()
