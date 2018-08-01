24660ebd461ba02d9045e58ab13a5c79fcbb043c
Unknown <mythologyali@gmail.com>
2018-08-01 12:01:20 -0700

    add: info fetch for youtube-dl

---
+++ server/youtube/download.py
@@ -0,0 +1,12 @@
+import youtube_dl
+from datetime import datetime
+from ..utils.logging import logger
+
+
+async def fetch_info(client: youtube_dl.YoutubeDL, song: str, ip: str = 'unknown user') -> dict:
+    logger.info(f'Got an extract request for {song} from {ip}')
+    start = datetime.now()
+    something = await client.extract_info(f'ytsearch:{song}', download=False)
+    delta = datetime.now() - start
+    logger.info(f'Extracted information for {song} in {delta}ms')
+    return something
