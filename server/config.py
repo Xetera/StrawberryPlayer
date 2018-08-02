ac514859fa7d2cecb608ca5cb2219d647d5c87eb
Unknown <mythologyali@gmail.com>
2018-08-02 01:34:27 -0700

    add: save location config

--- server/config.py
+++ server/config.py
@@ -1,3 +1,4 @@
+import os
 ytdl_opts = {
     'format': 'bestaudio/best',
     'postprocessors': [{
@@ -6,3 +7,12 @@ ytdl_opts = {
         'preferredquality': '140'
     }]
 }
+
+valid_requests = [
+    'search',
+    'download'
+]
+
+
+def get_save_location():
+    return os.environ.get('DOWNLOAD_LOCATION', '~/music')
