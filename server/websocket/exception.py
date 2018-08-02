0383829a3c3305717f8b4828421417f2de76be6b
Unknown <mythologyali@gmail.com>
2018-08-02 03:24:46 -0700

    fix: exception init

--- server/websocket/exception.py
+++ server/websocket/exception.py
@@ -4,6 +4,7 @@ from .packet import Packet
 class WebsocketRequestException(Packet):
     def __init__(self, **kwargs):
         super(
+        ).__init__(
             header=kwargs.get('header', 'error'),
             body=kwargs.get('body', 'Unexpected server error'),
             user='StrawberryServer'
