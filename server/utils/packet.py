6832892f1e31805727bb64570d169839995dc7a3
Unknown <mythologyali@gmail.com>
2018-08-02 03:26:19 -0700

    delete: stringify function

--- server/utils/packet.py
+++
@@ -1,8 +0,0 @@
-import json
-from .logging import logger
-
-
-def stringify(obj: dict) -> str:
-    out = json.dumps(obj)
-    logger.debug(f'Stringified an object to size {len(out)}')
-    return out
