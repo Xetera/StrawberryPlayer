3ed84121be3fa6968cac26a0586f8bee9f231e10
Unknown <mythologyali@gmail.com>
2018-08-04 04:41:22 -0700

    update: websocketService

--- web/src/app/websocket.service.ts
+++ web/src/app/websocket.service.ts
@@ -1,9 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import Hashids from 'hashids';
-import websocketConnect from 'rxjs-websockets';
-import {QueueingSubject} from 'queueing-subject';
-import {Observable} from 'rxjs';
+import {Observable, ReplaySubject, Subject} from 'rxjs';
+import {Packet} from './interfaces';
 
 @Injectable({
     providedIn: 'root'
@@ -11,34 +10,66 @@ import {Observable} from 'rxjs';
 export class WebsocketService {
     private hasher: Hashids;
     public user: string;
-    public queue: QueueingSubject<string>;
+    public outgoing$: ReplaySubject<Packet>;
+    public sock: WebSocket;
+    public incoming$: ReplaySubject<Packet>;
     public receiver: Observable<string>;
-    public connStatus: Observable<number>;
+    public connected = false;
+    public connection: Subject<boolean> = new Subject<boolean>();
+
     constructor(public http: HttpClient) {
         this.hasher = new Hashids('strawberry-player');
+        this.outgoing$ = new ReplaySubject<Packet>();
+        this.incoming$ = new ReplaySubject<Packet>();
         this.user = this.hasher.encode(Date.now());
-        this.queue = new QueueingSubject<string>();
-        ({messages: this.receiver, connectionStatus: this.connStatus} = websocketConnect('ws://localhost:10000', this.queue));
+        // ({messages: this.receiver, connectionStatus: this.connStatus} = websocketConnect('ws://localhost:10000', this.queue));
         this.registerHandlers();
+        this.registerListeners();
     }
 
     private createPacket = (event: string, body: string): Packet => ({event, body, user: this.user});
 
     private dispatch = (event: string, body: string): void => {
         const packet = this.createPacket(event, body);
-        const serialized = JSON.stringify(packet);
-        this.queue.next(serialized);
+        this.outgoing$.next(packet);
     }
 
     private registerHandlers = () => {
-        this.receiver.subscribe(message => console.log(message));
+        this.outgoing$.subscribe(packet => {
+            console.log('sending packet');
+            const serialized = JSON.stringify(packet);
+            this.sock.send(serialized);
+        });
+        this.incoming$.subscribe(packet => {
+            this.handleEvent(packet);
+        });
     }
 
-    private handleEvent(packet: MessageEvent) {
+    private registerListeners = () => {
+        this.sock = new WebSocket('ws://localhost:10000');
+        this.sock.onmessage = event => {
+            const packet: Packet = JSON.parse(event.data);
+            this.incoming$.next(packet);
+        };
+        this.sock.onopen = () => {
+            this.connected = true;
+            this.connection.next(true);
+        };
+        this.sock.onclose = () => {
+            this.connected = false;
+            setTimeout(() => this.registerListeners(), 4000);
+            this.connection.next(false);
+        };
+    }
+
+    public subscribeOutgoing = () => {
+        return this.outgoing$.asObservable();
+    }
+
+    private handleEvent(packet: Packet) {
     }
 
     public search = (search: string) => this.dispatch('download', search);
-
 
 
 }
